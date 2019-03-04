2ca5eaceb095c0457e57e5c8db0572213ef0332e
m <morgantosini@gmail.com>
2019-03-04 15:19:52 -0500

    Demo

---
+++ 250/Assets/Scripts/Movement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+
+    public Transform groundCheck;
+    public float groundCheckRadius = .1f;
+    public LayerMask whatIsGround;
+    public bool isGrounded;
+
+    public float moveSpeed = 10;
+    public float jumpForce = 10;
+    public float airSpeed = 6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        float x = Input.GetAxisRaw("Horizontal"); // * Time.deltaTime;
+
+        if (x > 0)
+        {
+            sprite.flipX = false;
+        }
+        else if (x < 0)
+        {
+            sprite.flipX = true;
+        }
+
+        if (isGrounded)
+        {
+            rb.velocity = new Vector2(x * moveSpeed, rb.velocity.y);
+        } else
+        {
+            rb.velocity = new Vector2(x * airSpeed, rb.velocity.y);
+        }
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+    }
+}
---
+++ 250/Assets/Scripts/Player Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+
+    public float moveSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime;
+
+        if (x > 0)
+        {
+            sprite.flipX = false;
+        }
+        else if (x < 0)
+        {
+            sprite.flipX = true;
+        }
+
+        rb.velocity = new Vector2(x * moveSpeed, rb.velocity.y);
+
+    }
+}
