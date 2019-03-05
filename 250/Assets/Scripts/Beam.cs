fddcabd95d939d283052d9e30af1a28c5f391793
m <morgantosini@gmail.com>
2019-03-05 13:35:26 -0500

    Beam firing

--- 250/Assets/Scripts/Beam.cs
+++ 250/Assets/Scripts/Beam.cs
@@ -5,16 +5,43 @@ using UnityEngine;
 public class Beam : MonoBehaviour
 {
     public float speed = 10;
+    public float lifetime = 4;
+    private Rigidbody2D rb;
+
+    public int l;
+    public Vector3 a;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+        //length = collision.contacts.Length;
+        //a = collision.contacts[0].normal;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        lifetime -= Time.deltaTime;
+        if (lifetime <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
         transform.Translate(new Vector2(speed * Time.deltaTime, 0)); 
+
+
+ 
     }
 }
--- 250/Assets/Scripts/Movement.cs
+++ 250/Assets/Scripts/Movement.cs
@@ -18,7 +18,72 @@ public class Movement : MonoBehaviour
     public float airSpeed = 6;
 
     public int direction;
+    public GameObject beam;
+    public float cooldown = .7f;
+    public float cdcount = 0;
 
+
+    void Shoot()
+    {
+        float n = 0;
+        if (sprite.flipX)
+        {
+            if (direction == 0)
+            {
+                n = 180;
+            }
+            else if (direction == 1)
+            {
+                n = 180;
+            }
+            else if (direction == 2)
+            {
+                n = 135;
+            }
+            else if (direction == 3)
+            {
+                n = 90;
+            }
+            else if (direction == 4)
+            {
+                n = 225;
+            }
+            else if (direction == 5)
+            {
+                n = 270;
+            }
+        }
+        else
+        {
+            if (direction == 0)
+            {
+                n = 0;
+            }
+            else if (direction == 1)
+            {
+                n = 0;
+            }
+            else if (direction == 2)
+            {
+                n = 45;
+            }
+            else if (direction == 3)
+            {
+                n = 90;
+            }
+            else if (direction == 4)
+            {
+                n = 315;
+            }
+            else if (direction == 5)
+            {
+                n = 270;
+            }
+        }
+
+        Quaternion q = Quaternion.Euler(0, 0, n);
+        Instantiate(beam, transform.position, q);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -33,6 +98,8 @@ public class Movement : MonoBehaviour
     {
 
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        cdcount -= Time.deltaTime;
 
         float x = Input.GetAxisRaw("Horizontal"); // * Time.deltaTime;
         float y = Input.GetAxisRaw("Vertical");
@@ -89,6 +156,15 @@ public class Movement : MonoBehaviour
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (cdcount <= 0)
+            {
+                Shoot();
+                cdcount = cooldown;
+            }
         }
 
     }
