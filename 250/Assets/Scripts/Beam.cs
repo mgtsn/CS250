f891333fb92772d0e7dd2fd8f3e3071388726b82
m <morgantosini@gmail.com>
2019-03-04 21:54:58 -0500

    v2

---
+++ 250/Assets/Scripts/Beam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beam : MonoBehaviour
+{
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector2(speed * Time.deltaTime, 0)); 
+    }
+}
--- 250/Assets/Scripts/Movement.cs
+++ 250/Assets/Scripts/Movement.cs
@@ -6,6 +6,7 @@ public class Movement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
+    private Animator anim; 
 
     public Transform groundCheck;
     public float groundCheckRadius = .1f;
@@ -16,11 +17,15 @@ public class Movement : MonoBehaviour
     public float jumpForce = 10;
     public float airSpeed = 6;
 
+    public int direction;
+
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -30,6 +35,38 @@ public class Movement : MonoBehaviour
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
 
         float x = Input.GetAxisRaw("Horizontal"); // * Time.deltaTime;
+        float y = Input.GetAxisRaw("Vertical");
+
+        if (x == 1 || x == -1)
+        {
+            if (y == 1)
+            {
+                direction = 2;
+            } else if (y == -1)
+            {
+                direction = 4;
+            } else
+            {
+                direction = 1;
+            }
+        } else if (x == 0)
+        {
+            if (y == 1)
+            {
+                direction = 3;
+            }
+            else if (y == -1)
+            {
+                direction = 5;
+            }
+            else
+            {
+                direction = 0;
+            }
+        }
+
+        anim.SetInteger("Dir", direction);
+
 
         if (x > 0)
         {
@@ -43,10 +80,12 @@ public class Movement : MonoBehaviour
         if (isGrounded)
         {
             rb.velocity = new Vector2(x * moveSpeed, rb.velocity.y);
-        } else
+        }
+        else
         {
             rb.velocity = new Vector2(x * airSpeed, rb.velocity.y);
         }
+
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
