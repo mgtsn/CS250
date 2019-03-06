6e50793d9d96996d2dc62fe3c96b94acbf7de0b9
m <morgantosini@gmail.com>
2019-03-05 20:39:43 -0500

    raycast

--- 250/Assets/Scripts/Beam.cs
+++ 250/Assets/Scripts/Beam.cs
@@ -7,27 +7,49 @@ public class Beam : MonoBehaviour
     public float speed = 10;
     public float lifetime = 4;
     private Rigidbody2D rb;
+    private Collider2D cd;
 
     public int l;
-    public Vector3 a;
+    public Vector2 a;
 
-    private void OnTriggerEnter2D(Collider2D other)
+    public Vector2 b;
+
+
+    void OnTriggerEnter2D(Collider2D other)
     {
+        ContactPoint2D[] c = new ContactPoint2D[10];
         if (other.gameObject.tag == "Ground")
         {
-            Destroy(this.gameObject);
+            a = other.Distance(cd).pointB;
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(-1, 0));
+            b = hit.normal;
+            print(hit.transform);
+            print(b);
+            //Destroy(this.gameObject);
         }
     }
 
+    /*void OnCollisionEnter2D(Collision2D collision)
+    {
 
-        //length = collision.contacts.Length;
-        //a = collision.contacts[0].normal;
-    
+        ContactPoint2D[] c = new ContactPoint2D[10];
+        print("Hit");
+        if (collision.gameObject.tag == "Ground")
+        {
+            collision.GetContacts(c);
+            print(c[0].normal);
+
+        }
+    }*/
+        
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        cd = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
